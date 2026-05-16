2edd58139bcb3ed1044ec7481d223641ab2c5304
aalkhalifa <aalkhalifa@gmail.com>
2026-05-16 04:26:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,8 @@
-const CACHE = ‘compost-v5’;
-const ASSETS = [
-‘/compost-logger/’,
-‘/compost-logger/index.html’,
-‘/compost-logger/manifest.json’,
-‘/compost-logger/icon-192.png’,
-‘/compost-logger/icon-512.png’
-];
-
-self.addEventListener(‘install’, e => {
-e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
-self.skipWaiting();
-});
-
+// Kill switch - unregister this service worker and clear all caches
+self.addEventListener(‘install’, () => self.skipWaiting());
 self.addEventListener(‘activate’, e => {
-e.waitUntil(caches.keys().then(keys =>
-Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-));
-self.clients.claim();
-});
-
-self.addEventListener(‘fetch’, e => {
-if (e.request.method !== ‘GET’) return;
-e.respondWith(
-fetch(e.request).then(res => {
-if (res.ok) {
-const clone = res.clone();
-caches.open(CACHE).then(c => c.put(e.request, clone));
-}
-return res;
-}).catch(() => caches.match(e.request))
+e.waitUntil(
+caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+.then(() => self.registration.unregister())
 );
 });

c9ad3c172375a564ffded66792c4be2df22cb217
aalkhalifa <aalkhalifa@gmail.com>
2026-05-16 03:34:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,33 @@
-const CACHE = 'compost-v3';
+const CACHE = ‘compost-v5’;
 const ASSETS = [
-  '/compost-logger/',
-  '/compost-logger/index.html',
-  '/compost-logger/manifest.json',
-  '/compost-logger/icon-192.png',
-  '/compost-logger/icon-512.png'
+‘/compost-logger/’,
+‘/compost-logger/index.html’,
+‘/compost-logger/manifest.json’,
+‘/compost-logger/icon-192.png’,
+‘/compost-logger/icon-512.png’
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
-  self.skipWaiting();
+self.addEventListener(‘install’, e => {
+e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener(‘activate’, e => {
+e.waitUntil(caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+));
+self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      const fresh = fetch(e.request).then(res => {
-        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-        return res;
-      }).catch(() => cached);
-      return cached || fresh;
-    })
-  );
+self.addEventListener(‘fetch’, e => {
+if (e.request.method !== ‘GET’) return;
+e.respondWith(
+fetch(e.request).then(res => {
+if (res.ok) {
+const clone = res.clone();
+caches.open(CACHE).then(c => c.put(e.request, clone));
+}
+return res;
+}).catch(() => caches.match(e.request))
+);
 });
